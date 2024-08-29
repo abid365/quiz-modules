f4037a362ffae4e4b00739eadbf861d75b2d8f2d
abid365 <abidibnazam@gmail.com>
2024-08-29 13:42:03 +0600

    saving progress

--- lib/pages/create_quiz_page.dart
+++ lib/pages/create_quiz_page.dart
@@ -142,212 +142,123 @@ class _CreateQuizPageState extends State<CreateQuizPage> {
       resizeToAvoidBottomInset: true,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(14),
-              ),
-              margin: const EdgeInsets.only(top: 30.0),
-              child: const Align(
-                alignment: Alignment.centerLeft, // Align to the left
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment
-                        .start, // Align children to the start of the column
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ..._qlist.asMap().entries.map((entry) {
+                int index = entry.key;
+                String item = entry.value;
+
+                if (item == 'Title&Description') {
+                  return Column(
                     children: [
-                      Text(
-                        "Test",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
+                      TitleAndDesc(
+                        titleAndDesc: titleAndDescription,
                       ),
                       Padding(
-                        padding: EdgeInsets.only(top: 10),
-                        child: Text(
-                          "Add necessary information to create new test.",
-                          style: TextStyle(fontSize: 14),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(0, 10, 0, 2),
-                        child: Divider(
-                          thickness: 1.0,
-                          color: Color.fromARGB(48, 158, 158, 158),
-                          height: 30,
+                        padding: const EdgeInsets.fromLTRB(0, 5, 0, 8),
+                        child: Row(
+                          children: [
+                            Transform.scale(
+                              scale: 0.7,
+                              child: Switch(
+                                value: isAutomaticGrading,
+                                onChanged: (value) {
+                                  setState(() {
+                                    isAutomaticGrading = value;
+                                  });
+                                },
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                              child: Text(
+                                "Automatic Grading",
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
-                  ),
+                  );
+                } else {
+                  print(index);
+
+                  return MultiQuestion(
+                    key: UniqueKey(),
+                    handleAddLastQuestion: () {},
+                    handleAddQuestion: _handleAddQuestion,
+                    handleDeleteQuestion: _handleDeleteQuestion,
+                    handleSelectedOption: _handleSelectedOption,
+                    handleCreateQuestion: _handleCreateQuestion,
+                    createQuestionJson: createdQuestionJSON,
+                    index: index,
+                    images: _selectedImages,
+                    experimentImagesMap: experimentImages,
+                    templateLength: _qlist.length,
+                    selectedAnswersMap: selectedAnswers,
+                    isAutoGradingOn: isAutomaticGrading,
+                  );
+                }
+              }).toList(),
+
+              // Adding the widgets after the list
+              const Divider(
+                thickness: 1.0,
+                color: Colors.grey,
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7),
+                          side: const BorderSide(color: Colors.black12),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "Cancel",
+                        style: TextStyle(
+                            color: Color(0xFF7F56D9),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF7F56D9),
+                        // backgroundColor: _isSubmitting
+                        //     ? Colors.grey
+                        //     : const Color(0xFF7F56D9),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7),
+                        ),
+                      ),
+                      onPressed: () {
+                        debugPrint('$createdQuestionJSON');
+                      },
+                      child: const Text(
+                        "Submit",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  controller: _scrollController,
-                  scrollDirection: Axis.vertical,
-                  physics: const AlwaysScrollableScrollPhysics(),
-                  itemCount: _qlist.length,
-                  itemBuilder: (context, index) {
-                    return _qlist[index] == 'Title&Description'
-                        ? Column(
-                            children: [
-                              TitleAndDesc(
-                                titleAndDesc: titleAndDescription,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(0, 5, 0, 8),
-                                child: Row(
-                                  children: [
-                                    Transform.scale(
-                                      scale: 0.7,
-                                      child: Switch(
-                                        value: isAutomaticGrading,
-                                        onChanged: (value) {
-                                          setState(
-                                            () {
-                                              isAutomaticGrading = value;
-                                            },
-                                          );
-                                        },
-                                      ),
-                                    ),
-                                    const Padding(
-                                      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                                      child: Text(
-                                        "Automatic Grading",
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.w600),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ],
-                          )
-                        : MultiQuestion(
-                            key: ValueKey(_qlist[index]),
-                            handleAddQuestion: _handleAddQuestion,
-                            handleDeleteQuestion: _handleDeleteQuestion,
-                            handleSelectedOption: _handleSelectedOption,
-                            handleCreateQuestion: _handleCreateQuestion,
-                            handleAddLastQuestion: _handleAddLastQuestion,
-                            createQuestionJson: createdQuestionJSON,
-                            index: index,
-                            images: _selectedImages,
-                            experimentImagesMap: experimentImages,
-                            templateLength: _qlist.length,
-                            selectedAnswersMap: selectedAnswers,
-                            isAutoGradingOn: isAutomaticGrading,
-                          );
-                  },
-                ),
-              ),
-            ),
-            if (_showSubmissionActions)
-              Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                    child: Divider(
-                      thickness: 1.0,
-                      color: Colors.grey,
-                      height: 20,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            elevation: 0,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(7),
-                              side: const BorderSide(color: Colors.black12),
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: const Text(
-                            "Cancel",
-                            style: TextStyle(
-                                color: Color(0xFF7F56D9),
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFF7F56D9),
-                            elevation: 0,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(7),
-                            ),
-                          ),
-                          onPressed: () {
-                            // if (title.isEmpty && description.isEmpty) {
-                            //   showDialog(
-                            //       context: context,
-                            //       builder: (context) => const UniversalToast(
-                            //             state: 'error',
-                            //             errorTitle:
-                            //                 'Title and Description required!',
-                            //             errMessage:
-                            //                 'Provide title and description',
-                            //           ));
-                            //   return;
-                            // } else {
-                            //   printTitleAndDescription();
-                            // }
-                            printTitleAndDescription();
-                            if (createdQuestionJSON.isEmpty) {
-                              showDialog(
-                                  context: context,
-                                  builder: (context) => const UniversalToast(
-                                        state: 'error',
-                                        errorTitle: 'Submission Error',
-                                        errMessage: 'No question was created',
-                                      ));
-                              return;
-                            }
-
-                            // debugPrint("List: $createdQuestionJSON");
-                            String jsonString = jsonEncode(createdQuestionJSON
-                                .map((q) => q.toJson())
-                                .toList());
-                            debugPrint('JSON:$jsonString');
-                            debugPrint('Selected Answers:$selectedAnswers');
-                            debugPrint('Images:$experimentImages');
-
-                            setState(() {
-                              _isSubmit = !_isSubmit;
-                            });
-                          },
-                          child: const Text(
-                            "Submit",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  )
-                ],
-              ),
-          ],
+            ],
+          ),
         ),
       ),
     );
